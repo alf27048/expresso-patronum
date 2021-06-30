616fcdbfe2473718e1256b485aa6b20d42b6f95c
alf27048 <analaurafarfan@gmail.com>
2021-06-29 21:01:10 -0300

    Magia pages y css

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {  //setea las paginas, en este caso / es la principal
+  res.render('index', { title: 'GATITOS' });  // cuando alguien ingrese a localhost 3000, quiero renderices index
 });
 
+router.get('/nosotros', (req, res) => {
+  res.render('pages/nosotros');
+});
+
+router.get('/contacto', (req, res) => {
+  res.render('pages/contacto');
+});
 module.exports = router;
