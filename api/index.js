1a04b0134aa10d9e42ff1652949d483f9287666e
alf27048 <analaurafarfan@gmail.com>
2021-07-03 11:31:32 -0300

    Listado de libros y pagina de libros

--- api/index.js
+++ api/index.js
@@ -9,13 +9,23 @@ const getBooks = async () => {  //guardo en variables funciones,para despues lla
         });
 
     return books;
-}  
+}
+
+const getBookById = async (id) => {
+    const book = await db.libro.findByPk(id, {include: db.autor}) // es un where mas directo, dame este libro que tiene este id
+        .then(result => {                   // busca por primary Key
+            return result
+        });
+return book;
+}
 
 
 const getAuthor = async () => {  //guardo en variables funciones,para despues llamarlas 
     //promesa
     //getBooks es una funcion, si la guardo en una variable puedop exportarla
-const author = await db.autor.findAll()    //llamada a la BD
+
+    //{include: db.autor} para traer lo datos del autor
+const author = await db.autor.findAll({include: db.autor})    //llamada a la BD
 .then(result => {
 return result;          // exporto los datos de la tabla
 });
@@ -23,5 +33,5 @@ return result;          // exporto los datos de la tabla
 return author;
 }  
 module.exports = {
-getAuthor, getBooks
+getAuthor, getBooks, getBookById
 }
--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,30 @@ router.get('/', async (req, res) => {
   const books = await api.getBooks();
   console.log(books);
 
-  //res.render('index', { title: 'Gatitos' });
+  res.render('index', { title: 'Libreria', books });  // para mandar la info en ejs
 
   // Devuelve un JSON con  la informacion
-  res.send(books);
+  //res.send(books);
 });
+
+/* router.get('/libro/:id', async (req, res) => {
+  //console.log(req.params);
+  const book = await api.getBookById(req.params.id);
+// views/pages/libro.ejs
+//aca mostramos, titulo completo, nombre autor y precio
+//res.render(....libro.ejs y le enviamos la data de book)
+  res.render('pages/libro', { book });
+
+ // res.send(`Vas bien!, el Id que buscas es ${req.params.id}`);
+ res.send(book);
+});*/
+
+router.get('/libro/:id', async (req, res) => {
+  // Los datos de la URL vienen en req.params
+  const book = await api.getBookById(req.params.id);
+  res.render('pages/libro', { book });
+});
+
 
 router.get('/autores', async (req, res) => {
   const author = await api.getAuthor();
